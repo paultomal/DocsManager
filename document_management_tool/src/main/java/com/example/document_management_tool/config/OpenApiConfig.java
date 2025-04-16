cf12fbd11d1d74ec5010ad7f469bea8a374abda2
paultomal <tamal.paul.42123@gmail.com>
2025-04-16 20:45:08 +0600

    Swagger added plus Migrate dependency to application.xml

---
+++ document_management_tool/src/main/java/com/example/document_management_tool/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.document_management_tool.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Document Management Tool", version = "v1"),
+        security = @SecurityRequirement(name = "Bearer Token")
+)
+@SecurityScheme(
+        name = "Bearer Token",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
